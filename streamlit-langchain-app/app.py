79b66b2fc5d8a8ee04f3e433f49e9ad1f5f2fdb9
ken5scal <kengoscal@gmail.com>
2023-11-05 19:03:23 +0900

    finish ch7

---
+++ streamlit-langchain-app/app.py
@@ -0,0 +1,71 @@
+import os
+import streamlit as st
+import json
+
+from dotenv import load_dotenv
+from langchain.chat_models import ChatOpenAI
+from langchain.schema import HumanMessage
+from langchain.agents import AgentType, initialize_agent, load_tools
+from langchain.callbacks import StreamlitCallbackHandler
+from langchain.memory import ConversationBufferMemory
+from langchain.prompts import MessagesPlaceholder
+
+from slack_bolt.adapter.aws_lambda import SlackRequestHandler
+
+def create_agent_chain():
+        chat = ChatOpenAI(
+                model_name=os.environ["OPENAI_API_MODEL"],
+                temperature=os.environ["OPENAI_API_TEMPERATURE"],
+                streaming=True,
+        )
+
+        agent_kwargs = {
+                "extra_prompt_messages": [MessagesPlaceholder(variable_name="memory")],
+        }
+        memory = ConversationBufferMemory(memory_key="memory", return_messages=True)
+
+        tools = load_tools(["ddg-search", "wikipedia"])
+        return initialize_agent(
+                tools, 
+                chat, 
+                agent=AgentType.OPENAI_FUNCTIONS,
+                agent_kwargs=agent_kwargs,
+                memory=memory,
+            )
+
+load_dotenv()
+
+st.title("langchain-streamlit-app")
+
+if "agent_chain" not in st.session_state:
+        st.session_state.agent_chain = create_agent_chain()
+
+if "messages"  not in st.session_state:
+        st.session_state.messages = []
+
+for message in st.session_state.messages:
+        with st.chat_message(message["role"]):
+                st.markdown(message["content"])
+
+prompt = st.chat_input("Say something")
+
+if prompt:
+        st.session_state.messages.append({"role": "user", "content": prompt})
+        with st.chat_message("user"):
+                st.markdown(prompt)
+        with st.chat_message("assistant"):
+                # chat = ChatOpenAI(
+                #         model_name=os.environ["OPENAI_API_MODEL"],
+                #         temperature=os.environ["OPENAI_API_TEMPERATURE"],
+                # )
+                # messages = [HumanMessage(content=prompt)]
+                # response=" Hello"
+                # response = chat(messages)
+                #st.markdown(response.content)
+                callback = StreamlitCallbackHandler(st.container())
+                # agentt_chain = create_agent_chain()
+                # response = agentt_chain.run(prompt, callbacks=[callback])
+                response = st.session_state.agent_chain.run(prompt, callbacks=[callback])
+                st.markdown(response)
+
+        st.session_state.messages.append({"role": "assistant", "content": response})
